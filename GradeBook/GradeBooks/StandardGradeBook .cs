54e033cdef19915b55dbb8120c97ca6916c7a094
Gaurav Rawat <rawatgaurav81@gmail.com>
2022-01-13 18:21:28 +0530

    First code pushed

---
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook:BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
